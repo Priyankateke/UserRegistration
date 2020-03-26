62ced90e674ba0247b9335993bb77b9f66e501b3
Priyankateke <priiyankateke1705@gmail.com>
2020-03-25 22:47:27 -0700

    ADDED: Validation Of Password Having Exactly One Special Character

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,13 +1,15 @@
 
 #!/bin/bash -x
 
+#used for on exterbnal globbing
+shopt -s extglob
+
 echo "Welcome To User Registration"
 
 #Patterns
 NAME_PATTERN="^[A-Z][a-z]{2,}$"
 EMAIL_PATTERN="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
 MOBILE_NUMBER_PATTERN="^[0-9]{1,3}[' '][0-9]{10}$"
-PASSWORD_AT_LEAST_ONE_UPPER_CASE_CHARACTER_ONE_NUMERIC="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)(.*[0-9].*{1}+)[a-zA-Z0-9]*{7,40}$"
 
 #Validating user details
 function validateUserDetails()
@@ -22,6 +24,18 @@ function validateUserMobileNumber()
 	[[ $mobileNumber =~ $MOBILE_NUMBER_PATTERN ]] && echo "Valid Mobile Number" || echo "Invalid Mobile Number"
 }
 
+#validating user password
+function validateUserPassword()
+{
+	read -p "Enter Password : " password
+	if [[ ${#password} -ge 8 && "$password" == *[[:lower:]]* && "$password" == *[[:upper:]]* && "$password" == *[0-9]*  && "$password" == *['!'@#\$%^\&*()_+]* ]]
+	then
+		echo "Valid Password"
+	else
+		echo "Invalid Password"
+	fi
+}
+
 read -p "Enter First Name : " firstName
 validateUserDetails $firstName $NAME_PATTERN
 
@@ -34,5 +48,4 @@ validateUserDetails $emailId $EMAIL_PATTERN
 read -p "Enter Mobile Number : " mobileNumber
 validateUserMobileNumber $mobileNumber $MOBILE_NUMBER_PATTERN
 
-read -p "Enter Password(At least 8 characters long and at least 1 upper case character, 1 numeric number)  : " password
-validateUserDetails $password $PASSWORD_AT_LEAST_ONE_UPPER_CASE_CHARACTER_ONE_NUMERIC
+validateUserPassword
